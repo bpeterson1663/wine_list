77fa8e4106222bf2a85c3a37cc0ad4558c8a9af4
Brady Peterson <bpeterson1663@gmail.com>
2021-04-09 23:11:58 -0500

    feat(wine list): create wine list component

---
+++ client/src/components/WineList.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { useQuery } from '@apollo/client'
+import { fetchAllWines } from '../queries/wines'
+import { WineT } from '../types/Wine'
+
+const WineList: React.FC = (): JSX.Element => {
+    const { loading, data, error } = useQuery(fetchAllWines)
+    if(loading) return <div>...Loading</div>
+    if(error) return <div>An error occured {error.message}</div>
+    return(
+        <div>
+            {data.wines.map( (wine: WineT)  => (
+                <div key={wine.id}>
+                    {wine.name}
+                </div>
+            ))}
+        </div>
+    )
+}
+
+export default WineList
