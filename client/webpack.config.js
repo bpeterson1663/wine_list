96ce4dd2a71a401cbe1c841be47674405f5d7362
Brady Peterson <bpeterson1663@gmail.com>
2021-04-09 22:19:51 -0500

    feat(webpack): configure webpack for development

---
+++ client/webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.tsx',
+    module: {
+        rules: [
+            {
+              test: /\.tsx?$/,
+              exclude: /node_modules/,
+              loader: 'babel-loader',
+            }
+        ]
+    },
+    resolve: { extensions: ['.ts', '.tsx', '.js'] },
+    output: {
+        path: path.resolve(__dirname, 'dist/'),
+        publicPath: '/dist/',
+        filename: 'bundle.js'
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'public/'),
+        compress: true,
+        port: 9000,
+    },
+    plugins: [new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'public', 'index.html') })]
+
+}
