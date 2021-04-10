4d030ee1bba9768b99761d57e931dbc158cefbe3
Brady Peterson <bpeterson1663@gmail.com>
2021-04-10 16:37:09 -0500

    feat(sql): query live data from postgres db

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,6 +1,8 @@
 const graphql = require('graphql')
 const WineType = require('./types/WineType')
 const { Client } = require('pg')
+const { selectAllWines } = require('../sql/queries') 
+
 const client = new Client({
     host: '0.0.0.0',
     port: 5432
@@ -12,40 +14,22 @@ client.connect(err => {
       console.log('connected')
     }
   })
+
 const {
     GraphQLObjectType,
     GraphQLSchema,
     GraphQLList
 } = graphql
 
-const DB = [
-    {
-        name: 'Dummy Wine',
-        id: '1',
-        winery: 'Dummy Winery',
-        vintage: '2020'
-    },
-    {
-        name: 'Dummy Wine 2',
-        id: '2',
-        winery: 'Dummy Winery 2',
-        vintage: '2020'
-    },
-    {
-        name: 'Dummy Wine 3',
-        id: '3',
-        winery: 'Dummy Winery 3',
-        vintage: '2019'
-    },
-    
-]
 const RootQuery = new GraphQLObjectType({
     name: 'RootQueryType',
     fields: {
         wines: {
             type: new GraphQLList(WineType),
-            resolve: (parentValue, args) => {
-                return DB
+            resolve: () => {
+                return client.query(selectAllWines).then(res => {
+                    return res.rows
+                })
             }
         }
     }
