eadb2dd36d42c4cff6ff4f8fda7f5dbc047e6dc7
Brady Peterson <bpeterson1663@gmail.com>
2021-04-09 23:12:16 -0500

    feat(apollo): implement apollo client and inject wine list component

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,4 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import {
+    ApolloClient, ApolloProvider, InMemoryCache
+} from '@apollo/client'
+import WineList from './components/WineList'
+const client = new ApolloClient({
+    uri: 'http://localhost:4000/graphql',
+    cache: new InMemoryCache()
+})
 
-ReactDOM.render(<h1>Wine List</h1>, document.getElementById('root'))
+const Root = () => {
+    return (
+        <ApolloProvider client={client}>
+            <h1>Wine List</h1>
+            <WineList />
+        </ApolloProvider>
+    )
+}
+ReactDOM.render(<Root />, document.getElementById('root'))
