a819d133441e9d47d1e5e794c8944a60bcb7d3bc
Brady Peterson <bpeterson1663@gmail.com>
2021-04-11 15:29:05 -0500

    feat(eslint): install and configure eslint

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,29 +1,29 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-    mode: 'development',
-    entry: './src/index.tsx',
-    module: {
-        rules: [
-            {
-              test: /\.tsx?$/,
-              exclude: /node_modules/,
-              loader: 'babel-loader',
-            }
-        ]
-    },
-    resolve: { extensions: ['.ts', '.tsx', '.js'] },
-    output: {
-        path: path.resolve(__dirname, 'dist/'),
-        publicPath: '/',
-        filename: 'bundle.js'
-    },
-    devServer: {
-        contentBase: path.join(__dirname, '/'),
-        compress: true,
-        port: 9000,
-    },
-    plugins: [new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'index.html') })]
-
+  mode: 'development',
+  entry: './src/index.tsx',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+    ],
+  },
+  resolve: { extensions: ['.ts', '.tsx', '.js'] },
+  output: {
+    path: path.resolve(__dirname, 'dist/'),
+    publicPath: '/',
+    filename: 'bundle.js',
+  },
+  devServer: {
+    contentBase: path.join(__dirname, '/'),
+    compress: true,
+    port: 9000,
+  },
+  plugins: [new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'index.html') })],
 }
