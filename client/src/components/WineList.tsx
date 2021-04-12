1bbc09d679f847d955e7c44f3917a38cd862d239
Brady Peterson <bpeterson1663@gmail.com>
2021-04-11 23:08:40 -0500

    feat(wine list): display wine categories using material ui

--- client/src/components/WineList.tsx
+++ client/src/components/WineList.tsx
@@ -1,22 +1,54 @@
 import React from 'react'
 import { useQuery } from '@apollo/client'
+import { makeStyles } from '@material-ui/core/styles'
+import { GridList, GridListTile, GridListTileBar, IconButton } from '@material-ui/core'
+import InfoIcon from '@material-ui/icons/Info'
 import { fetchAllVarietals } from '../queries/wines'
 import Varietal from '../models/Varietal'
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+    overflow: 'hidden',
+    backgroundColor: theme.palette.background.paper,
+  },
+  gridList: {
+    width: 900,
+  },
+  icon: {
+    color: 'rgba(255, 255, 255, 0.54)',
+  }
+}))
 const WineList: React.FC = (): JSX.Element => {
   const { loading, data, error } = useQuery(fetchAllVarietals)
+  const classes = useStyles()
+
   if (loading) return <div>...Loading</div>
   if (error) return <div>An error occured {error.message}</div>
+
   const varietals: Varietal[] = [new Varietal('All Varietals')]
   data.varietals.forEach((varietal: Varietal) => varietals.push(new Varietal(varietal.varietal)))
+
   return (
-    <div>
+    <div className={classes.root}>
+      <GridList cellHeight={180} className={classes.gridList}>
       {varietals.map((wine: Varietal) => (
-        <div key={wine.varietal}>
-          {wine.varietal}
-          <img src={wine.image} width={200} height={200} />
-        </div>
+        <GridListTile key={wine.varietal}>
+         <img src={wine.image} alt={wine.varietal} />
+            <GridListTileBar
+              title={wine.varietal}
+              actionIcon={
+                <IconButton aria-label={`info about ${wine.varietal}`} className={classes.icon}>
+                  <InfoIcon />
+                </IconButton>
+              }
+            />
+        </GridListTile>
       ))}
-    </div>
+    </GridList>
+      </div>
   )
 }
 
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
+import { Typography } from '@material-ui/core'
 import WineList from './components/WineList'
 const client = new ApolloClient({
   uri: 'http://localhost:4000/graphql',
@@ -10,7 +11,7 @@ const client = new ApolloClient({
 const Root = () => {
   return (
     <ApolloProvider client={client}>
-      <h1>Wine List</h1>
+      <Typography variant="h2" component="h2">Wine List</Typography>
       <WineList />
     </ApolloProvider>
   )
