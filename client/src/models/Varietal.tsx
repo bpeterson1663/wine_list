fdb7f35256bbbda1f1ebb5d9340dd3d0f3ba6ae4
Brady Peterson <bpeterson1663@gmail.com>
2021-04-12 07:48:58 -0500

    refactor(varietal): refactor varietal to use getter for image

--- client/src/models/Varietal.tsx
+++ client/src/models/Varietal.tsx
@@ -1,15 +1,13 @@
 import VARIETALS from '../constants/Varietal.list'
 
 export default class Varietal {
-  image: string
   varietal: string
   constructor(varietal: string) {
     this.varietal = varietal
-    this.image = this.setImage(varietal)
   }
 
-  setImage(varietal: string): string {
-    const grape = VARIETALS.find((grape) => grape.varietal === varietal)
+  get image(): string {
+    const grape = VARIETALS.find((grape) => grape.varietal === this.varietal)
     return grape
       ? grape.image
       : 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5c/Vi%C3%B1edoCafayate.jpg/1200px-Vi%C3%B1edoCafayate.jpg'
