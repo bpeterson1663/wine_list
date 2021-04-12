a2ee4eb10691deeac0507d0e830e720a89e757cd
Brady Peterson <bpeterson1663@gmail.com>
2021-04-11 20:09:17 -0500

    feat(varietal list): create varietal list constant

---
+++ client/src/constants/Varietal.list.tsx
@@ -0,0 +1,36 @@
+export default [
+  {
+    varietal: 'Sauvignon Blanc',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/Sauvignon_blanc_grapes.jpg/220px-Sauvignon_blanc_grapes.jpg',
+    description: '',
+  },
+  {
+    varietal: 'Semillon',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Semillon_wine_grapes.jpg/220px-Semillon_wine_grapes.jpg',
+    description: '',
+  },
+  {
+    varietal: 'Cabernet Sauvignon',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/3/32/Red_Mountain_Cabernet_Sauvignon_grapes_from_Hedge_Vineyards.jpg/220px-Red_Mountain_Cabernet_Sauvignon_grapes_from_Hedge_Vineyards.jpg',
+    description: '',
+  },
+  {
+    varietal: 'Petit Verdot',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/f/fa/01_Petit_verdot.jpg',
+    description: '',
+  },
+  {
+    varietal: 'Chardonnay',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b3/Chardonnay_grapes_close_up.jpg/200px-Chardonnay_grapes_close_up.jpg',
+    description: '',
+  },
+  {
+    varietal: 'Malbec',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a2/Malbec_grapes.jpg/1200px-Malbec_grapes.jpg',
+    description: '',
+  },
+]
