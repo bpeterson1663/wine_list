7a08170b231f3c8b2898fcc787bd35315ba4d99e
Brady Peterson <bpeterson1663@gmail.com>
2021-04-13 22:52:15 -0500

    feat(theme): implement material ui theme provider

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -4,13 +4,22 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
 import ListByVarietal from './components/ListByVarietal'
 import Navigation from './components/Navigation'
+import { ThemeProvider } from '@material-ui/styles'
+import { createMuiTheme } from '@material-ui/core/styles'
+
 const client = new ApolloClient({
   uri: 'http://localhost:4000/graphql',
   cache: new InMemoryCache(),
 })
-
+const theme = createMuiTheme({
+  palette: {
+    primary: { main: '#1588E6' },
+    secondary: { main: '#DC567F' },
+  },
+})
 const Root = () => {
   return (
+    <ThemeProvider theme={theme}>
       <ApolloProvider client={client}>
         <BrowserRouter>
           <Navigation />
@@ -21,6 +30,7 @@ const Root = () => {
           </Switch>
         </BrowserRouter>
       </ApolloProvider>
+    </ThemeProvider>
   )
 }
 ReactDOM.render(<Root />, document.getElementById('root'))
