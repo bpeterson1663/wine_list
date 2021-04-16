4a2f5615395f28f4eb8c22b68d880a5c57ea2975
Brady Peterson <bpeterson1663@gmail.com>
2021-04-15 21:51:07 -0500

    feat(wine): styling updates and go back button

--- client/src/components/WineInfo.tsx
+++ client/src/components/WineInfo.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
-import { Paper } from '@material-ui/core'
+import PropTypes from 'prop-types'
+import { Paper, Container, IconButton } from '@material-ui/core'
 import { useQuery, gql } from '@apollo/client'
 import { useParams } from 'react-router-dom'
 import { makeStyles } from '@material-ui/core/styles'
-
+import ArrowBackIcon from '@material-ui/icons/ArrowBack'
 const fetchWine = gql`
   query wines($id: String!) {
     wines(id: $id) {
@@ -22,8 +23,22 @@ const useStyles = makeStyles(() => ({
     width: '120px',
     height: '400px',
   },
+  container: {
+    display: 'flex',
+    justifyContent: 'center',
+  },
+  paperContainer: {
+    margin: 5,
+    padding: '5px 10px',
+    maxWidth: 400,
+  },
 }))
-const WineInfo: React.FC = (): JSX.Element => {
+
+interface WineInfoT {
+  history: { goBack: () => void }
+}
+
+const WineInfo: React.FC<WineInfoT> = ({ history }): JSX.Element => {
   const { id } = useParams()
   const { loading, data, error } = useQuery(fetchWine, {
     variables: { id },
@@ -34,16 +49,29 @@ const WineInfo: React.FC = (): JSX.Element => {
 
   const wine = data.wines[0]
   return (
-    <Paper>
-      <h3>
-        {wine.winery} {wine.name}
-      </h3>
-      <h4>{wine.vintage}</h4>
-      <h5>${wine.price}</h5>
-      <img className={classes.wineBottle} src={wine.image} alt={wine.varietal} />
-      <p>{wine.description}</p>
-    </Paper>
+    <>
+      <IconButton onClick={() => history.goBack()}>
+        <ArrowBackIcon />
+      </IconButton>
+      <Container className={classes.container}>
+        <Paper elevation={0} className={classes.paperContainer}>
+          <img className={classes.wineBottle} src={wine.image} alt={wine.varietal} />
+        </Paper>
+        <Paper elevation={3} className={classes.paperContainer}>
+          <h3>{wine.winery}</h3>
+          <h3>{wine.name}</h3>
+          <h3>{wine.varietal}</h3>
+          <h4>{wine.vintage}</h4>
+          <h5>${wine.price}</h5>
+          <p>{wine.description}</p>
+        </Paper>
+      </Container>
+    </>
   )
 }
-
+WineInfo.propTypes = {
+  history: PropTypes.shape({
+    goBack: PropTypes.func.isRequired,
+  }).isRequired,
+}
 export default WineInfo
