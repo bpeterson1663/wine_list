0fadc3eab7666fadcc78bf0874350d1ab4f2a2c5
Brady Peterson <bpeterson1663@gmail.com>
2021-04-18 20:01:59 -0500

    feat(wine grid): implement wine grid component

--- client/src/components/Varietals/ListByVarietal.tsx
+++ client/src/components/Varietals/ListByVarietal.tsx
@@ -1,12 +1,10 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { useLocation, Link } from 'react-router-dom'
+import { useLocation } from 'react-router-dom'
 import { useQuery, gql } from '@apollo/client'
-import { makeStyles } from '@material-ui/core/styles'
-import { Container, GridList, GridListTile, GridListTileBar, IconButton } from '@material-ui/core'
-import { WineT } from '../../types/types'
-import InfoIcon from '@material-ui/icons/Info'
+import { IconButton } from '@material-ui/core'
 import ArrowBackIcon from '@material-ui/icons/ArrowBack'
+import WineGridList from '../Common/WineGridList'
 
 const fetchWinesByGrape = gql`
   query wines($varietal: String!) {
@@ -19,30 +17,6 @@ const fetchWinesByGrape = gql`
     }
   }
 `
-const useStyles = makeStyles((theme) => ({
-  root: {
-    display: 'flex',
-    flexWrap: 'wrap',
-    justifyContent: 'space-around',
-    overflow: 'hidden',
-    backgroundColor: theme.palette.background.paper,
-  },
-  gridList: {
-    width: 900,
-  },
-  imageContainer: {
-    display: 'flex',
-    width: '100%',
-    justifyContent: 'center',
-  },
-  icon: {
-    color: 'rgba(255, 255, 255, 0.54)',
-  },
-  wineBottle: {
-    width: '120px',
-    height: '400px',
-  },
-}))
 
 function useQueryParam() {
   return new URLSearchParams(useLocation().search)
@@ -53,7 +27,6 @@ interface ListByVarietalT {
 }
 
 const ListByVarietal: React.FC<ListByVarietalT> = ({ history }): JSX.Element => {
-  const classes = useStyles()
   const query = useQueryParam()
 
   const { loading, data, error } = useQuery(fetchWinesByGrape, {
@@ -66,27 +39,7 @@ const ListByVarietal: React.FC<ListByVarietalT> = ({ history }): JSX.Element => 
       <IconButton onClick={() => history.goBack()}>
         <ArrowBackIcon />
       </IconButton>
-      <Container className={classes.root}>
-        <GridList cellHeight={400} className={classes.gridList}>
-          {data.wines.map((wine: WineT) => (
-            <GridListTile key={wine.id}>
-              <div className={classes.imageContainer}>
-                <img className={classes.wineBottle} src={wine.image} alt={wine.varietal} />
-              </div>
-              <GridListTileBar
-                title={`${wine.name} - ${wine.winery}`}
-                actionIcon={
-                  <Link to={`/wine/${wine.id}`}>
-                    <IconButton aria-label={`info about ${wine.name}`} className={classes.icon}>
-                      <InfoIcon />
-                    </IconButton>
-                  </Link>
-                }
-              />
-            </GridListTile>
-          ))}
-        </GridList>
-      </Container>
+      <WineGridList data={data.wines} />
     </>
   )
 }
--- client/src/components/Vintage/ListByVintage.tsx
+++ client/src/components/Vintage/ListByVintage.tsx
@@ -1,12 +1,11 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { makeStyles } from '@material-ui/core/styles'
-import { useLocation, Link } from 'react-router-dom'
+import { useLocation } from 'react-router-dom'
 import { useQuery, gql } from '@apollo/client'
-import { Container, GridList, GridListTile, GridListTileBar, IconButton } from '@material-ui/core'
-import InfoIcon from '@material-ui/icons/Info'
+import { IconButton } from '@material-ui/core'
 import ArrowBackIcon from '@material-ui/icons/ArrowBack'
-import { WineT } from '../../types/types'
+import WineGridList from '../Common/WineGridList'
 
 const fetchWinesByVintage = gql`
   query wines($vintage: String!) {
@@ -64,27 +63,7 @@ const ListByVintage: React.FC<ListByVintageT> = ({ history }): JSX.Element => {
       <IconButton onClick={() => history.goBack()}>
         <ArrowBackIcon />
       </IconButton>
-      <Container className={classes.root}>
-        <GridList cellHeight={400} className={classes.gridList}>
-          {data.wines.map((wine: WineT) => (
-            <GridListTile key={wine.id}>
-              <div className={classes.imageContainer}>
-                <img className={classes.wineBottle} src={wine.image} alt={wine.varietal} />
-              </div>
-              <GridListTileBar
-                title={`${wine.name} - ${wine.winery}`}
-                actionIcon={
-                  <Link to={`/wine/${wine.id}`}>
-                    <IconButton aria-label={`info about ${wine.name}`} className={classes.icon}>
-                      <InfoIcon />
-                    </IconButton>
-                  </Link>
-                }
-              />
-            </GridListTile>
-          ))}
-        </GridList>
-      </Container>
+      <WineGridList data={data.wines} />
     </>
   )
 }
