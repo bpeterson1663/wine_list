0aae06e4195b008cba155cbffbbba11653252b3f
Brady Peterson <bpeterson1663@gmail.com>
2021-04-26 23:46:44 -0500

    fix(image url): fix image url being undefined

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -12,7 +12,7 @@ const {
   selectWineById,
   getVarietals,
   getVintages,
-  getRegions
+  getRegions,
 } = require('../sql/queries')
 
 const { GraphQLObjectType, GraphQLSchema, GraphQLList, GraphQLString } = graphql
@@ -32,11 +32,11 @@ const RootQuery = new GraphQLObjectType({
         if (args.varietal) {
           return selectWineByVarietal(args.varietal).then((res) => res.items)
         } else if (args.region) {
-          return selectWineByRegion(args.region)
+          return selectWineByRegion(args.region).then((res) => res.items)
         } else if (args.vintage) {
-          return selectWineByVintage(args.vintage)
+          return selectWineByVintage(args.vintage).then((res) => res.items)
         } else if (args.id) {
-          return selectWineById(args.id)
+          return selectWineById(args.id).then((res) => res.items)
         } else {
           return selectAllWines()
         }
@@ -45,19 +45,19 @@ const RootQuery = new GraphQLObjectType({
     varietals: {
       type: new GraphQLList(VarietalType),
       resolve: () => {
-        return getVarietals().then(res => res.items)
+        return getVarietals().then((res) => res.items)
       },
     },
     vintages: {
       type: new GraphQLList(VintageType),
       resolve: () => {
-        return getVintages().then(res => res.items)
+        return getVintages().then((res) => res.items)
       },
     },
     regions: {
       type: new GraphQLList(RegionType),
       resolve: () => {
-        return getRegions().then(res => res.items)
+        return getRegions().then((res) => res.items)
       },
     },
   },
--- server/sql/queries.js
+++ server/sql/queries.js
@@ -2,73 +2,81 @@ const mongodb = require('mongodb')
 const ObjectId = mongodb.ObjectId
 const Wine = require('../db/models/Wine')
 
+const transformWines = (wines) => {
+  return wines.map((wine) => {
+    const wineItem = wine.toObject()
+    wineItem.imageUrl = decodeURIComponent(wineItem.imageUrl)
+    wineItem.id = wineItem._id
+    return wineItem
+  })
+}
+
 const selectAllWines = async () => {
   const response = await Wine.find({})
+  const items = transformWines(response)
   return {
-    items: response,
+    items: items,
   }
 }
 
 const selectWineByVarietal = async (varietal) => {
   const response = await Wine.find({ varietal: varietal })
+  const items = transformWines(response)
   return {
-    items: response,
+    items: items,
   }
 }
 
-const selectWineByRegion = (region) => {
-  return Wine.find({ region: region }, (err, items) => {
-    if (err) {
-      return { success: false, error: err }
-    }
-    return { success: true, items: items }
-  })
+const selectWineByRegion = async (region) => {
+  const response = await Wine.find({ region: region })
+  const items = transformWines(response)
+  return {
+    items: items,
+  }
 }
 
-const selectWineByVintage = (vintage) => {
-  return Wine.find({ vintage: vintage }, (err, items) => {
-    if (err) {
-      return { success: false, error: err }
-    }
-    return { success: true, items: items }
-  })
+const selectWineByVintage = async (vintage) => {
+  const response = await Wine.find({ vintage: vintage })
+  const items = transformWines(response)
+  return {
+    items: items,
+  }
 }
 
-const selectWineById = (id) => {
-  return Wine.find({ _id: new ObjectId(id) }, (err, items) => {
-    if (err) {
-      return { success: false, error: err }
-    }
-    return { success: true, items: items }
-  })
+const selectWineById = async (id) => {
+  const response = await Wine.find({ _id: new ObjectId(id) })
+  const items = transformWines(response)
+  return {
+    items: items,
+  }
 }
 
 const getVarietals = async () => {
-    const response = await Wine.distinct('varietal')
-    return {
-      items: response.map(item => {
-        return { varietal: item }
-      }),
-    }
+  const response = await Wine.distinct('varietal')
+  return {
+    items: response.map((item) => {
+      return { varietal: item }
+    }),
   }
-  
-  const getVintages = async () => {
-    const response = await Wine.distinct('vintage')
-    return {
-      items: response.map(item => {
-        return { vintage: item }
-      })
-    }
+}
+
+const getVintages = async () => {
+  const response = await Wine.distinct('vintage')
+  return {
+    items: response.map((item) => {
+      return { vintage: item }
+    }),
   }
-  
-  const getRegions = async () => {
-    const response = await Wine.distinct('region')
-    return {
-      items: response.map(item => {
-        return { region: item }
-      })
-    }
+}
+
+const getRegions = async () => {
+  const response = await Wine.distinct('region')
+  return {
+    items: response.map((item) => {
+      return { region: item }
+    }),
   }
+}
 module.exports = {
   selectAllWines,
   selectWineByVarietal,
@@ -77,5 +85,5 @@ module.exports = {
   selectWineById,
   getVarietals,
   getVintages,
-  getRegions
+  getRegions,
 }
