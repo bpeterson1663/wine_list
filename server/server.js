b61e86211a3ecb619766fc22846e95bed78df8f0
Brady Peterson <bpeterson1663@gmail.com>
2021-07-29 20:35:40 -0500

    fix(cors): allow cors between graphql and client

--- server/server.js
+++ server/server.js
@@ -1,9 +1,15 @@
 require('dotenv').config()
 const express = require('express')
+const cors = require('cors')
 const { graphqlHTTP } = require('express-graphql')
 const schema = require('./schema/schema')
 const app = express()
 const db = require('./db/index')
+app.use(
+  cors({
+    origin: ['http://localhost:9000'], // <-- location of the react app for development
+  }),
+)
 app.use(
   '/graphql',
   graphqlHTTP({
